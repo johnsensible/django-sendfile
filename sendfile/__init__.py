6bd5000eccaa32c5a2e9328aecd80f4f5f641744
John Montgomery <john@sensibledevelopment.com>
2010-11-05 11:24:09 +0000

    Content-disposition/attachment now works

--- sendfile/__init__.py
+++ sendfile/__init__.py
@@ -1,6 +1,7 @@
 VERSION = (0, 0, 1)
 __version__ = '.'.join(map(str, VERSION))
 
+import os.path
 
 def _lazy_load(fn):
     _cached = []
@@ -26,10 +27,17 @@ def _get_sendfile():
 
 
 
-def sendfile(request, filename, attachment_name=None):
+def sendfile(request, filename, attachment=False, attachment_filename=None):
+    '''
+    create a response to send file using backend configured in SENDFILE_BACKEND
+
+    if attachment is True the content-disposition header will be set with either
+    the filename given or else the attachment_filename (of specified).  This
+    will typically prompt the user to download the file, rather than view it.
+    '''
     _sendfile = _get_sendfile()
     response = _sendfile(request, filename)
-    if attachment_name is not None:
-        pass
-        # TODO set attachment disposition etc
+    if attachment:
+        attachment_filename = attachment_filename or os.path.basename(filename)
+        response['Content-Disposition'] = 'attachment; filename=%s' % attachment_filename
     return response
