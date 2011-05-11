1b3470f622a544b513056e703170485d11b4cda6
John Montgomery <john@sensibledevelopment.com>
2011-05-11 13:12:23 +0100

    Fixed tests
    Failing due to use of __file__, which was sometimes resolving to the pyc
    file instead of py file. Now referencing README.rst to avoid confusion.

--- sendfile/tests.py
+++ sendfile/tests.py
@@ -10,6 +10,9 @@ def sendfile(request, filename, **kwargs):
     return HttpResponse(filename)
 
 
+def _get_readme():
+    return os.path.join(os.path.dirname(os.path.dirname(__file__)), 'README.rst')
+
 class TestSendfile(TestCase):
 
     def setUp(self):
@@ -23,28 +26,28 @@ class TestSendfile(TestCase):
             pass
 
     def test_sendfile(self):
-        response = real_sendfile(HttpRequest(), __file__)
+        response = real_sendfile(HttpRequest(), _get_readme())
         self.assertTrue(response is not None)
-        self.assertEqual('text/x-python', response['Content-Type'])
-        self.assertEqual(__file__, response.content)
+        self.assertEqual('application/octet-stream', response['Content-Type'])
+        self.assertEqual(_get_readme(), response.content)
 
     def test_set_mimetype(self):
-        response = real_sendfile(HttpRequest(), __file__, mimetype='text/plain')
+        response = real_sendfile(HttpRequest(), _get_readme(), mimetype='text/plain')
         self.assertTrue(response is not None)
         self.assertEqual('text/plain', response['Content-Type'])
 
     def test_set_encoding(self):
-        response = real_sendfile(HttpRequest(), __file__, encoding='utf8')
+        response = real_sendfile(HttpRequest(), _get_readme(), encoding='utf8')
         self.assertTrue(response is not None)
         self.assertEqual('utf8', response['Content-Encoding'])
 
     def test_attachment(self):
-        response = real_sendfile(HttpRequest(), __file__, attachment=True)
+        response = real_sendfile(HttpRequest(), _get_readme(), attachment=True)
         self.assertTrue(response is not None)
-        self.assertEqual('attachment; filename=tests.py', response['Content-Disposition'])
+        self.assertEqual('attachment; filename=README.rst', response['Content-Disposition'])
 
     def test_attachment_filename(self):
-        response = real_sendfile(HttpRequest(), __file__, attachment=True, attachment_filename='tests.txt')
+        response = real_sendfile(HttpRequest(), _get_readme(), attachment=True, attachment_filename='tests.txt')
         self.assertTrue(response is not None)
         self.assertEqual('attachment; filename=tests.txt', response['Content-Disposition'])
 
