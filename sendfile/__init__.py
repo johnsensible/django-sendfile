67d998efb856cb60ed4f62cf114be9b6be82dd32
Karl Hobley <karlhobley10@gmail.com>
2015-04-14 10:25:36 +0100

    Fix typo

--- sendfile/__init__.py
+++ sendfile/__init__.py
@@ -26,7 +26,7 @@ def _get_sendfile():
 
     backend = getattr(settings, 'SENDFILE_BACKEND', None)
     if not backend:
-        raise ImproperlyConfigured('You must specify a valued for SENDFILE_BACKEND')
+        raise ImproperlyConfigured('You must specify a value for SENDFILE_BACKEND')
     module = import_module(backend)
     return module.sendfile
 
