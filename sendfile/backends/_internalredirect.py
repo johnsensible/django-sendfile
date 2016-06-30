7841251c4ed50c5ce3531778f3442107668565ff
Moritz Pfeiffer <moritz.pfeiffer@alp-phone.ch>
2016-06-30 12:18:44 +0200

    Added url quoting to url written to X-Accel-Redirect and Location headers.

--- sendfile/backends/_internalredirect.py
+++ sendfile/backends/_internalredirect.py
@@ -1,5 +1,12 @@
-from django.conf import settings
 import os.path
+
+from django.conf import settings
+from django.utils.encoding import force_text, force_bytes
+
+try:
+    from urllib.parse import quote
+except ImportError:
+    from urllib import quote
 
 
 def _convert_file_to_url(filename):
@@ -11,4 +18,7 @@ def _convert_file_to_url(filename):
         relpath, head = os.path.split(relpath)
         url.insert(1, head)
 
-    return u'/'.join(url)
+    # Python3 urllib.parse.quote accepts both unicode and bytes, while Python2 urllib.quote only accepts bytes.
+    # So force bytes for quoting and then go back to unicode.
+    url = [force_bytes(url_component) for url_component in url]
+    return force_text(quote(b'/'.join(url)))
