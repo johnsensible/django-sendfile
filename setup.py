b30328e815ddee346af671619ccf1cb9479d6367
Gilles Dartiguelongue <g.dartiguelongue@lexfo.fr>
2017-04-10 13:28:45 +0200

    Reduce support to currently supported Django versions
    As declared upstream at https://www.djangoproject.com/download/#supported-versions
    
    Update Trove classifiers and requires/install_requires as a consequence.

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
     url='https://github.com/johnsensible/django-sendfile',
     license='BSD',
 
-    requires=['Django (>=1.3)'],
-    install_requires=['Django>=1.3'],
+    requires=['Django (>=1.8)'],
+    install_requires=['Django>=1.8'],
 
     packages=['sendfile', 'sendfile.backends'],
     package_dir={
@@ -31,15 +31,19 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
