817e025ff9aa82066a978a9771f83cbc663c83d5
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-30 13:41:54 +0200

    Fix manage.py

--- examples/protected_downloads/manage.py
+++ examples/protected_downloads/manage.py
@@ -2,13 +2,15 @@
 
 from __future__ import absolute_import
 
-from django.core.management import execute_manager
-try:
-    from . import settings  # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+#!/usr/bin/env python
+import os
+import sys
+
+# Add ../multiseek to sys.path
+
+sys.path = [ os.path.join(os.path.dirname(__file__), '..', '..'), ] + sys.path
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
