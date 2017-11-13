20d8a2c1ff2e6ab6948baab0d85d60973d653dbc
Gilles Dartiguelongue <g.dartiguelongue@lexfo.fr>
2017-11-13 15:37:02 +0100

    Fix declaration of applications URLs in unittests
    Make protected_download app known as download through the AppConfig
    mechanism. Inspired by Michał Pasternak
    <mpasternak@MacBook-Pro-Michala.local> commits in PR#66.

--- examples/protected_downloads/example/urls.py
+++ examples/protected_downloads/example/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import url, include
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^', include('protected_downloads.download.urls')),
-    (r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^', include('download.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]
