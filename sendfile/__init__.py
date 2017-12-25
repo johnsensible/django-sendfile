b838474b59fb04385897c4e58079f48e8250b868
David Fischer <david.fischer.ch@gmail.com>
2017-12-25 11:39:59 +0100

    Fix PEP-8 issues

--- sendfile/__init__.py
+++ sendfile/__init__.py
@@ -35,8 +35,8 @@ def _get_sendfile():
 
 def sendfile(request, filename, check_exist=False, attachment=False, attachment_filename=None,
              mimetype=None, encoding=None):
-    '''
-    create a response to send file using backend configured in SENDFILE_BACKEND
+    """
+    Create a response to send file using backend configured in SENDFILE_BACKEND.
 
     If attachment is True the content-disposition header will be set.
     This will typically prompt the user to download the file, rather
@@ -49,7 +49,7 @@ def sendfile(request, filename, check_exist=False, attachment=False, attachment_
 
     If no mimetype or encoding are specified, then they will be guessed via the
     filename (using the standard python mimetypes module)
-    '''
+    """
     _sendfile = _get_sendfile()
 
     if check_exist and not os.path.exists(filename):
